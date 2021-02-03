fe259069787e004764dd1d31ccf4d94190b0d62a
Pierre-Loïc <pierreloic.pichon@gmail.com>
2021-02-03 14:49:33 +0100

    Stop script after Oh-My-Zsh script

--- setup.sh
+++ setup.sh
@@ -54,8 +54,3 @@ sudo tlp start
 printf "\nInstalling Oh-My-Zsh and plugins...\n"
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-printf "\nVisit https://extensions.gnome.org/extension/307/dash-to-dock/ to customize Dock!\n"
