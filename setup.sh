8c85548d35d723f5c1d970a166cf922e23f886af
Pierre-Loïc <pierreloic.pichon@gmail.com>
2021-02-03 14:55:34 +0100

    Style

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-printf "\nInstalling ZSH plugins...\n\n"
+printf "\n >>>>> Installing ZSH plugins...\n\n"
 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-printf "\nVisit https://extensions.gnome.org/extension/307/dash-to-dock/ to customize Dock!\n"
+printf "\n >>>>> Visit https://extensions.gnome.org/extension/307/dash-to-dock/ to customize Dock!\n"
